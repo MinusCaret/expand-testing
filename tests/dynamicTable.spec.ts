bdc4f7d74286b5bb31ef3b79ce46b186568d91f2
Anthropolis <247051419+MinusCaret@users.noreply.github.com>
2026-01-17 16:45:27 +1300

    tidied up tests folder

--- tests/dynamicTable.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '../fixtures/test';
-
-test.beforeEach(async ({page}) => {
-    await page.goto('https://practice.expandtesting.com/dynamic-table')
-})
-
--- tests/forms/checkbox.spec.ts
+++ tests/forms/checkbox.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '../fixtures/test';
+import { test, expect } from '../../fixtures/test';
 
 test.beforeEach(async ({ page }) => {
     await page.goto('https://practice.expandtesting.com/checkboxes')
