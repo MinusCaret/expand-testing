dba23bb58c681ccb50ce7a57d6816b89075c1583
Anthropolis <247051419+MinusCaret@users.noreply.github.com>
2026-03-09 10:34:55 +1300

    Update products.spec.ts
    Added GET and POST tests.

--- tests/api/products.spec.ts
+++ tests/api/products.spec.ts
@@ -1,24 +1,81 @@
 import { test, expect } from '@playwright/test'
-
-// test.beforeEach(async ({page}) => {
-//     await page.goto('https://fakestoreapi.com/docs')
-// })
+import { faker } from '@faker-js/faker'
 
 test('GET /products returns product list', async ({ request }) => {
-  const res = await request.get('/products/')
+    const response = await request.get('/products')
 
-  expect(res.status()).toBe(200)
+    expect(response.ok()).toBeTruthy()
 
-  const products = await res.json()
-  expect(products).toBeInstanceOf(Array)
-  expect(products.length).toBeGreaterThan(0)
+    const products = await response.json()
+    expect(products).toBeInstanceOf(Array)
+    expect(products.length).toBeGreaterThan(0)
 
-  expect(products[0]).toMatchObject({
+    expect(products[0]).toMatchObject({
     id: expect.any(Number),
     title: expect.any(String),
     price: expect.any(Number),
     category: expect.any(String),
   })
-
-  //console.log(products)
+    console.log(products)
 })
+
+test('GET /products/{id} returns single product', async ({ request }) => {
+    const randomId = Math.floor(Math.random() * 20) + 1
+    const response = await request.get(`/products/${randomId}`)
+
+    expect(response.ok()).toBeTruthy()
+
+    const product = await response.json()
+
+    expect(product).not.toBeInstanceOf(Array)
+    expect(typeof product).toBe('object')
+
+    expect(product.id).toBe(randomId)
+
+    expect(product).toMatchObject({
+    id: expect.any(Number),
+    title: expect.any(String),
+    price: expect.any(Number),
+    description: expect.any(String),
+    category: expect.any(String),
+    image: expect.any(String),
+    rating: expect.objectContaining({
+        rate: expect.any(Number),
+        count: expect.any(Number)
+    })
+  })
+    console.log(product)
+})
+
+test('POST /products creates a new product', async ({ request }) => {
+    const newProductData = {
+        title: faker.commerce.productName(),
+        price: faker.number.float({ fractionDigits: 2, min: 5, max: 100 }),
+        description: faker.commerce.productDescription(),
+        category: faker.commerce.product(), //api accepts anything as long as it's a string, otherwise use an array
+        image: faker.image.url()
+    }
+
+    const response = await request.post('/products', {
+        data: newProductData
+    })
+
+    expect(response.ok()).toBeTruthy()
+
+    const responseBody = await response.json()
+    console.log(responseBody)
+
+    expect(responseBody).toMatchObject({
+        id: expect.any(Number),
+        title: newProductData.title,
+        price: newProductData.price
+    })
+})
+
+test('PUT /products/{id} updates product by ID', async ({ request }) => {
+
+})
+
+test('DEL /products/{id} deletes specific product by ID', async ({ request }) => {
+
+}) 
